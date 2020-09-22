672e7c341516494df3c79b2870d1609a4b631798
Natalia Lebedeva <natalia.lebedeva@t-systems.com>
2020-09-22 11:45:46 +0300

    Autocomplete: focusing always starts with the first entry

--- __ivy_ngcc__/fesm2015/ng-zorro-antd-auto-complete.js
+++ __ivy_ngcc__/fesm2015/ng-zorro-antd-auto-complete.js
@@ -371,9 +371,9 @@ class NzAutocompleteComponent {
     setActiveItem(index) {
         /** @type {?} */
         const activeItem = this.options.toArray()[index];
+        this.activeItemIndex = index;
         if (activeItem && !activeItem.active) {
             this.activeItem = activeItem;
-            this.activeItemIndex = index;
             this.clearSelectedOptions(this.activeItem);
             this.activeItem.setActiveStyles();
             this.changeDetectorRef.markForCheck();
--- bundles/ng-zorro-antd-auto-complete.umd.js
+++ bundles/ng-zorro-antd-auto-complete.umd.js
@@ -565,9 +565,9 @@
         function (index) {
             /** @type {?} */
             var activeItem = this.options.toArray()[index];
+            this.activeItemIndex = index;
             if (activeItem && !activeItem.active) {
                 this.activeItem = activeItem;
-                this.activeItemIndex = index;
                 this.clearSelectedOptions(this.activeItem);
                 this.activeItem.setActiveStyles();
                 this.changeDetectorRef.markForCheck();
