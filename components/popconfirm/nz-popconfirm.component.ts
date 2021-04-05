93fadb74dace00ca8f5fb2733f5a15f222cc0de2
Vlad Fedorenkov <vladislav.fedorenkov@t-systems.com>
2021-04-05 17:38:20 +0300

    Popconfirm: focus-trap

--- components/popconfirm/nz-popconfirm.component.ts
+++ components/popconfirm/nz-popconfirm.component.ts
@@ -16,11 +16,14 @@ import {
   Optional,
   Output,
   TemplateRef,
+  ViewChild,
   ViewEncapsulation
 } from '@angular/core';
 
 import { zoomBigMotion, InputBoolean, NzNoAnimationDirective } from 'ng-zorro-antd/core';
 import { NzTooltipBaseComponentLegacy, NzTooltipTrigger, NzToolTipComponent } from 'ng-zorro-antd/tooltip';
+import { ElementRef } from '@angular/core';
+import { FocusTrapFactory, FocusTrap } from '@angular/cdk/a11y';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -54,29 +57,48 @@ export class NzPopconfirmComponent extends NzToolTipComponent {
   @Output() readonly nzOnCancel: EventEmitter<void> = new EventEmitter();
   @Output() readonly nzOnConfirm: EventEmitter<void> = new EventEmitter();
 
+  // some stuff for focus-trap (a11y):
+  @ViewChild('popoverContainer', { static: false })
+  popoverContainer: ElementRef;
+  private focusTrap: FocusTrap | null = null;
+  private previouslyFocusedElement: HTMLElement | null;
+
   _prefix = 'ant-popover-placement';
   _trigger: NzTooltipTrigger = 'click';
   _hasBackdrop = true;
 
-  constructor(cdr: ChangeDetectorRef, @Host() @Optional() public noAnimation?: NzNoAnimationDirective) {
+  constructor(
+    cdr: ChangeDetectorRef,
+    private focusTrapFactory: FocusTrapFactory,
+    @Host() @Optional() public noAnimation?: NzNoAnimationDirective,
+  ) {
     super(cdr, noAnimation);
   }
 
   show(): void {
     if (!this.nzCondition) {
+      this.previouslyFocusedElement = document.activeElement as HTMLElement;
       super.show();
+      this.focusTrap = this.focusTrapFactory.create(this.popoverContainer.nativeElement);
+      this.focusTrap.focusFirstTabbableElementWhenReady();
     } else {
       this.onConfirm();
     }
   }
 
+  close(): void {
+    this.focusTrap && this.focusTrap.destroy();
+    super.hide();
+    this.previouslyFocusedElement && this.previouslyFocusedElement.focus();
+  }
+
   onCancel(): void {
     this.nzOnCancel.emit();
-    super.hide();
+    this.close();
   }
 
   onConfirm(): void {
     this.nzOnConfirm.emit();
-    super.hide();
+    this.close();
   }
 }
